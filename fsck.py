3d697b3ff43f7e2c766538e1bb9cc34645f16e45
Tammy <zhouyuanquaner@hotmail.com>
2016-01-13 20:34:37 -0500

    Update fsck.py

--- fsck.py
+++ fsck.py
@@ -1,3 +1,18 @@
+REQUIREMENTS:
+	1)	The DeviceID is correct
+2)	All times are in the past, nothing in the future
+3)	Validate that the free block list is accurate this includes
+	a.	Making sure the free block list contains ALL of the free blocks
+	b.	Make sure than there are no files/directories stored on items listed in the free block list
+4)	Each directory contains . and .. and their block numbers are correct ( note: parent block should be right
+5)	Each directory’s link count matches the number of links in the filename_to_inode_dict
+6)	If the data contained in a location pointer is an array, that indirect is one
+7)	That the size is valid for the number of block pointers in the location array. The three possibilities are:
+	a.	size<blocksize if  indirect=0 and size>0
+	b.	size<blocksize*length of location array if indirect!=0
+	c.	size>blocksize*(length of location array-1) if indirect !=0
+
+
 import time
 
 current_time = int(time.time())
